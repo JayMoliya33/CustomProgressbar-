52c410fe92eb73555f29c7aa5a0a8e884696e730
Jay Moliya <jaymoliya33200@gmail.com>
2020-09-02 16:52:02 +0530

    updated

--- app/src/main/java/com/example/customprogressbar/MainActivity.java
+++ app/src/main/java/com/example/customprogressbar/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.customprogressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 
 import com.leo.simplearcloader.ArcConfiguration;
 import com.leo.simplearcloader.SimpleArcDialog;
+import com.leo.simplearcloader.SimpleArcLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void showProgress(View view) {
+    public void simplearc(View view) {
         SimpleArcDialog dialog = new SimpleArcDialog(this);
         dialog.setConfiguration(new ArcConfiguration(this));
+        dialog.setTitle("Loading...");
+        dialog.show();
+    }
+
+    public void simplearcCustomColor(View view) {
+        SimpleArcDialog dialog = new SimpleArcDialog(this);
+        ArcConfiguration configuration = new ArcConfiguration(this);
+        configuration.setColors(getResources().getIntArray(R.array.color_1));
+        configuration.setAnimationSpeed(8);
+        configuration.setArcMargin(5);
+        dialog.setConfiguration(configuration);
+        dialog.setTitle("Loading...");
+        dialog.show();
+    }
+
+    public void completeArc(View view) {
+        SimpleArcDialog dialog = new SimpleArcDialog(this);
+        ArcConfiguration configuration = new ArcConfiguration(this);
+        configuration.setLoaderStyle(SimpleArcLoader.STYLE.COMPLETE_ARC);
+        configuration.setAnimationSpeed(8);
+        configuration.setArcMargin(5);
+        dialog.setConfiguration(configuration);
         dialog.setTitle("Loading...");
         dialog.show();
     }
